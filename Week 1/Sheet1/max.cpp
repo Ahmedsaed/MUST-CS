54f974133c70d6894c69dd4f6974b5ed93673b3c
Ahmedsaed <ahmedsaed2652003@gmail.com>
2021-10-22 14:57:32 +0200

    Q1 Done

---
+++ Week 1/Sheet1/max.cpp
@@ -0,0 +1,42 @@
+// Write an algorithm to find and print the maximum of given three numbers.
+
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    int x, y, z;
+    cout << "First number: ";
+    cin >> x;
+
+    cout << "Second number: ";
+    cin >> y;
+
+    cout << "Third number: ";
+    cin >> z;
+
+    cout << "Max: ";
+    if (x > y)
+    {
+        if (x > z)
+        {
+            cout << x;
+        }
+        else
+        {
+            cout << z;
+        }
+    }
+    else
+    {
+        if (y > z)
+        {
+            cout << y;
+        }
+        else
+        {
+            cout << z;
+        }
+    }
+}
