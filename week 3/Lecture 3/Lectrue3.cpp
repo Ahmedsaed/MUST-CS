97bc10665a8920eeca984a65b49c9ff49cfa8bc4
Ahmedsaed <ahmedsaed2652003@gmail.com>
2021-10-29 14:51:01 +0200

    Lecture 3

---
+++ week 3/Lecture 3/Lectrue3.cpp
@@ -0,0 +1,180 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    int x, y, z;
+    double n;
+    char ch;
+
+    // cin >> x >> y >> z;
+    
+    // cin >> x;
+    // cin >> y;
+    // cin >> z;
+
+    // -----------------
+
+    // cin >> ch;
+    // cin >> x;
+
+    // cout << "Character: " << ch << endl;
+    // cout << "Intger: " << x;
+
+    // ------------------
+
+    // cout << "write a character? ";
+    // cin >> ch;
+    // cout << ch;
+
+    // --------------------
+
+    // cout << "write a number? ";
+    // cin >> n;
+    // cout << n;
+
+    // ---------------------
+    
+    // cout << "write two numbers?";
+    // cin >> x >> y;
+    // z = pow(x, y);
+    // cout << z;
+
+    // ------------------------
+
+    // cin >> x;
+    // cout << x;
+
+    // cin.get();
+    // cout << x;
+
+    // ---------------------------
+
+    // char name[100]; // is equivalent to: string name;
+
+    // cout << "What's your name?";
+
+    // cin.get(name, 100);
+    // cout << name << endl;
+
+    // cin >> name;
+    // cout << name;
+
+    // ----------------------------
+
+    /* TODO: Implement the example above using String data type */
+
+
+    // ----------------------------- cin.ignore(int, char);
+
+    /* TODO: Remove the middle name */
+
+    // char First_name[10]; // is equivalent to: string First_name;
+    // char middle_name[10]; // is equivalent to: string middle_name;
+    // char family_name[10]; // is equivalent to: string middle_name;
+
+    // cout << "What's your Full Name?";
+
+    // cin >> First_name;
+    // cout << "Your First Name: " << First_name << endl;
+
+    // cin >> middle_name;
+    // cout << "Your Middle Name: " << middle_name << endl;
+
+    // cin >> family_name;
+    // cout << "Your Family Name: " << family_name << endl;
+
+    // ----------------------------- setprecision(n)
+
+    // n = 22.0 / 7;
+
+    // cout << n;
+
+    // cout << setprecision(2);
+    
+    // cout << fixed << setprecision(2);
+    
+    // ----------------------------- setw(n)
+
+    // x = 30;
+
+    // cout << setw(5) << x << endl;
+
+    // cout << setw(5) << setfill('*') << x << endl;
+
+    // cout << setw(5) << setfill('*') << left << x << endl;
+
+    // ----------------------------- strings without blanks
+    
+    // string name;
+
+    // cout << "what's your name?";
+
+    // cin >> name;
+
+    // cout << "your name is " << name;
+
+    //  ----------------------------- strings with blanks
+
+    // string name;
+
+    // cout << "what's your name?";
+
+    // getline(cin, name);
+
+    // cout << "your name is " << name;
+
+    // ------------------------------ File I/O
+
+    // ifstream inData;
+    // ofstream outData;
+
+    // inData.open("names.data");
+
+    // string data;
+
+    // getline(inData, data);
+    // cout << data << endl;
+
+    // getline(inData, data);
+    // cout << data << endl;
+
+    // getline(inData, data);
+    // cout << data;
+
+    // inData.close();
+
+    // ---------------------------------
+
+    // TODO: Save the output to final.out 
+
+    // ifstream client_names;
+    // ifstream client_numbers;
+    // ofstream final;
+
+    // client_names.open("names.data");
+    // client_numbers.open("numbers.data");
+
+    // string name;
+    // string number;
+
+    // getline(client_names, name);
+    // getline(client_numbers, number);
+    // cout << name << ": " << number << endl;
+
+    // getline(client_names, name);
+    // getline(client_numbers, number);
+    // cout << name << ": " << number << endl;
+
+    // getline(client_names, name);
+    // getline(client_numbers, number);
+    // cout << name << ": " << number << endl;
+
+    // client_names.close();
+    // client_numbers.close();
+}
