70472dc5380daf72e421d1ecebf148945a56cf27
Ahmedsaed <ahmedsaed2652003@gmail.com>
2021-12-29 20:25:10 +0200

    add a better answer

--- week 11/Sheet 8/question6.cpp
+++ week 11/Sheet 8/question6.cpp
@@ -16,10 +16,20 @@ int main()
         alpha[i] = 3 * i;
     }
 
+    for (int i = 0; i < 50; i++)
+    {
+        if (i % 10 == 0)
+            cout << endl;
+        
+        cout << alpha[i] << " ";
+    }
+
+    /* // does the same as the above for loop but slightly more complicated 
     for (int i = 0; i < 50;)
     {
         for (int n = i + 10; i < n; i++)
             cout << alpha[i] << " ";
         cout << endl;
     }
+    */
 }
