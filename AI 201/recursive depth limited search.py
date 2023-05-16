682b030b8dfc5ca0b47a8560fc8a042bc3f7a036
Ahmed Saed <37080003+Ahmedsaed@users.noreply.github.com>
2023-05-16 21:55:18 +0300

    recursive depth limited search.py

---
+++ AI 201/recursive depth limited search.py
@@ -0,0 +1,48 @@
+romania_map = {
+    'Arad': {'Zerind': 75, 'Sibiu': 140, 'Timisoara': 118},
+    'Zerind': {'Arad': 75, 'Oradea': 71},
+    'Oradea': {'Zerind': 71, 'Sibiu': 151},
+    'Sibiu': {'Arad': 140, 'Oradea': 151, 'Fagaras': 99, 'Rimnicu': 80},
+    'Timisoara': {'Arad': 118, 'Lugoj': 111},
+    'Lugoj': {'Timisoara': 111, 'Mehadia': 70},
+    'Mehadia': {'Lugoj': 70, 'Drobeta': 75},
+    'Drobeta': {'Mehadia': 75, 'Craiova': 120},
+    'Craiova': {'Drobeta': 120, 'Rimnicu': 146, 'Pitesti': 138},
+    'Rimnicu': {'Sibiu': 80, 'Craiova': 146, 'Pitesti': 97},
+    'Fagaras': {'Sibiu': 99, 'Bucharest': 211},
+    'Pitesti': {'Rimnicu': 97, 'Craiova': 138, 'Bucharest': 101},
+    'Bucharest': {'Fagaras': 211, 'Pitesti': 101, 'Giurgiu': 90, 'Urziceni': 85},
+    'Giurgiu': {'Bucharest': 90},
+    'Urziceni': {'Bucharest': 85, 'Vaslui': 142, 'Hirsova': 98},
+    'Hirsova': {'Urziceni': 98, 'Eforie': 86},
+    'Eforie': {'Hirsova': 86},
+    'Vaslui': {'Urziceni': 142, 'Iasi': 92},
+    'Iasi': {'Vaslui': 92, 'Neamt': 87},
+    'Neamt': {'Iasi': 87}
+}
+
+def depth_limited_search(node, goal, depth_limit):
+    if node == goal:
+        return [node]  # Goal found, return the path
+
+    if depth_limit == 0:
+        return []  # Reached depth limit, return empty path
+
+    for child, distance in romania_map[node].items():
+        result = depth_limited_search(child, goal, depth_limit - 1)
+        if result:
+            return [node] + result  # Append current node to the path and return
+
+    return []  # Goal not found at this depth, return empty path
+
+# Example usage
+start_node = 'Arad'
+goal_node = 'Bucharest'
+limit = 4
+
+path = depth_limited_search(start_node, goal_node, limit)
+
+if path:
+    print("Path found:", ' -> '.join(path))
+else:
+    print("Path not found within the depth limit.")
